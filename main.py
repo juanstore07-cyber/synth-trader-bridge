21b837a880a65ef3732d1b4206b47543d75f41dd
juanstore07-cyber <juanstore07@gmail.com>
2026-02-22 10:49:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -92,9 +92,3 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
 ```
-
----
-
-Y el `Procfile` cámbialo a:
-```
-web: python main.py
